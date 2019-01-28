5418c22ac0b4ed7db319524daf8214cf31a27917
PlavorSeol <pseol2190@outlook.com>
2019-01-28 15:04:15 +0900

    Update LocalSettings.php

--- MediaWiki/LocalSettings.php
+++ MediaWiki/LocalSettings.php
@@ -1,15 +1,21 @@
 <?php
-/*Debug
-error_reporting(-1);
-ini_set("display_errors",1);
+
+##Debugging
+
+/*
 $wgShowDBErrorBacktrace=true;
 $wgShowExceptionDetails=true;
 $wgShowSQLErrors=true;
+error_reporting(-1);
+ini_set("display_errors",1);
 //*/
 
-/*Prevent web access*/
+##Prevent web access
+
 if (!defined("MEDIAWIKI"))
-{exit;}
+{die("You don't have permission to do that.");}
+
+##Wiki selector
 
 if ($wgCommandLineMode)
 {//For updating
@@ -21,8 +27,7 @@ else
   $wiki_code="central";
   break;
   default:
-  //$_SERVER["HTTPS"]
-  header("Location: http://{$_SERVER["HTTP_HOST"]}/error/403.html");
+  die("You don't have permission to do that.");
   exit;}
 }
 
@@ -30,14 +35,29 @@ $central_wiki_code="central";
 $data_dir="{$IP}/data";
 $private_data_dir="{$IP}/private_data";
 
-require_once("https://raw.githubusercontent.com/PlavorMind/Configurations/Main/MediaWiki/data/global_settings.php");
-require_once("https://raw.githubusercontent.com/PlavorMind/Configurations/Main/MediaWiki/data/extra_settings.php");
-include_once("{$data_dir}/{$wiki_code}/settings.php");
+##Settings
 
-/*
+/*Load settings*/
+//Global settings
+include_once("{$data_dir}/global_settings.php");
+include_once("{$data_dir}/extra_settings.php");
+//Per-wiki settings
+include_once("{$data_dir}/{$wiki_code}/settings.php");
+include_once("{$data_dir}/{$wiki_code}/extra_settings.php");
+
+##Appending settings
+
+/*Groups*/
+//Remove groups
+$wgExtensionFunctions[]=function() use (&$wgGroupPermissions)
+{unset($wgGroupPermissions["bot"]);
+unset($wgGroupPermissions["sysop"]);};
+//Temporary groups
 $wgGroupPermissions["bot"]["read"]=true;
-$wgGroupPermissions["interface-admin"]["read"]=true;
 $wgGroupPermissions["sysop"]["read"]=true;
-$wgGroupPermissions["bureaucrat"]["userrights"]=true;
+/*
+$wgGroupPermissions["interface-admin"]["read"]=true;
+$wgAddGroups["bureaucrat"]=["steward"];
+$wgRemoveGroups["bureaucrat"]=["bot","interface-admin","sysop"];
 //*/
 ?>
