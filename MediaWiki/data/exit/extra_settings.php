b613e907292e194b43e4eeeeaf6d7f559975f019
PlavorSeol <pseol2190@outlook.com>
2019-01-29 10:51:40 +0900

    Create extra_settings.php

---
+++ MediaWiki/data/exit/extra_settings.php
@@ -0,0 +1,18 @@
+<?php
+##Prevent web access
+
+if (!defined("MEDIAWIKI"))
+{die("You don't have permission to do that.");}
+
+##Extensions
+
+/*Renameuser*/
+wfLoadExtension("Renameuser");
+$wgGroupPermissions["bureaucrat"]["renameuser"]=false;
+$wgGroupPermissions["steward"]["renameuser"]=true;
+
+/*UserPageEditProtection*/
+include_once("{$wgExtensionDirectory}/UserPageEditProtection/UserPageEditProtection.php");
+$wgOnlyUserEditUserPage=true;
+//Staffs
+$wgGroupPermissions["staff"]["editalluserpages"]=true;
