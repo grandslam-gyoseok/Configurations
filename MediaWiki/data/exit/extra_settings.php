cbedb5c38697d8abb973314a0aa5452b72feba05
PlavorSeol <pseol2190@outlook.com>
2019-03-11 21:01:29 +0900

    Merge to settings.php
    But keep this file until it is no longer required

--- MediaWiki/data/exit/extra_settings.php
+++ MediaWiki/data/exit/extra_settings.php
@@ -3,22 +3,4 @@
 
 if (!defined("MEDIAWIKI"))
 {die("You don't have permission to do that.");}
-
-##Extensions
-
-/*Other extensions*/
-wfLoadExtensions(["CodeEditor","WikiEditor"]);
-
-##Skins
-
-/*Default skin*/
-$wgDefaultSkin="Timeless";
-
-##Appending settings
-
-/*Permission inheritance*/
-$wgGroupPermissions["staff"]+=$wgGroupPermissions["autoconfirmed"];
-$wgGroupPermissions["admin"]+=$wgGroupPermissions["staff"];
-$wgGroupPermissions["bureaucrat"]+=$wgGroupPermissions["admin"];
-$wgGroupPermissions["steward"]+=$wgGroupPermissions["bureaucrat"];
 ?>
