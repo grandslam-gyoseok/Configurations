32e938109eb4c13e14564d7ecafb0710520f8893
PlavorSeol <pseol2190@outlook.com>
2019-03-13 00:29:25 +0900

    <-CodeEditor <-WikiEditor

--- MediaWiki/data/exit/extra_settings.php
+++ MediaWiki/data/exit/extra_settings.php
@@ -4,21 +4,8 @@
 if (!defined("MEDIAWIKI"))
 {die("You don't have permission to do that.");}
 
-##Extensions
-
-/*Other extensions*/
-wfLoadExtensions(["CodeEditor","WikiEditor"]);
-
 ##Skins
 
 /*Default skin*/
 $wgDefaultSkin="Timeless";
-
-##Appending settings
-
-/*Permission inheritance*/
-$wgGroupPermissions["staff"]+=$wgGroupPermissions["autoconfirmed"];
-$wgGroupPermissions["admin"]+=$wgGroupPermissions["staff"];
-$wgGroupPermissions["bureaucrat"]+=$wgGroupPermissions["admin"];
-$wgGroupPermissions["steward"]+=$wgGroupPermissions["bureaucrat"];
 ?>
