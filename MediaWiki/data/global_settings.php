6d0f8496addbef8a841fbe568d9d15590f9f2944
PlavorSeol <pseol2190@outlook.com>
2019-04-21 01:31:49 +0900

    Fix $wgAutoblockExpiry

--- MediaWiki/data/global_settings.php
+++ MediaWiki/data/global_settings.php
@@ -21,7 +21,7 @@ $wgSitename="Wiki";
 
 /*Blocking*/
 $wgApplyIpBlocksToXff=true;
-$wgAutoblockExpiry=60*60*24*365*10; //10 years
+$wgAutoblockExpiry=60*60*24*365; //1 year
 $wgBlockCIDRLimit=
 ["IPv4"=>8, //###.0.0.0/8
 "IPv6"=>16]; //####::/16
