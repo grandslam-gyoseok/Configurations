e1d54be17f218ef7a91b2207a348e9d2bdb4d54c
PlavorSeol <pseol2190@outlook.com>
2019-05-11 02:22:24 +0900

    Create phpinfo.php

---
+++ Web/main/phpinfo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta content="initial-scale=1,user-scalable=no,width=device-width" name="viewport">
+    <style>
+table
+{max-width: 100%;}
+    </style>
+    <title>PHP information</title>
+  </head>
+  <body>
+
+<?php phpinfo(); ?>
+
+  </body>
+</html>
