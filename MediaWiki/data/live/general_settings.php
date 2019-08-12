20efc02129b6f0109e6bdb43446f44a4b3ef5730
PlavorSeol <pseol2190@outlook.com>
2019-08-12 17:44:35 +0900

    Add files via upload

---
+++ MediaWiki/data/live/general_settings.php
@@ -0,0 +1,11 @@
+<?php
+##Prevent web access
+
+if (!defined("MEDIAWIKI"))
+{exit("You don't have permission to access to this page.");}
+
+##General
+
+/*Basic information*/
+$wgSitename="Live위키";
+?>
