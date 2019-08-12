43046d05bd7bd9381a498addc55a5622666e52a4
PlavorSeol <pseol2190@outlook.com>
2019-08-13 01:05:57 +0900

    Update general_settings.php

--- MediaWiki/data/live/general_settings.php
+++ MediaWiki/data/live/general_settings.php
@@ -8,4 +8,35 @@ if (!defined("MEDIAWIKI"))
 
 /*Basic information*/
 $wgSitename="Live위키";
+
+/*Interface*/
+$wgForceUIMsgAsContentMsg=
+["excontent",
+"excontentauthor",
+"modifiedarticleprotection-comment",
+"protect-expiry-indefinite",
+"protect-fallback",
+"protect-level-editprotected",
+"protect-level-editprotected-admin",
+"protect-level-editprotected-autoconfirmed",
+"protect-level-editprotected-bureaucrat",
+"protect-level-editprotected-staff",
+"protect-level-editprotected-steward",
+"protect-level-editprotected-user",
+"protect-level-editsemiprotected",
+"protect-summary-cascade",
+"protect-summary-desc",
+"protectedarticle-comment",
+"restriction-delete",
+"restriction-edit",
+"restriction-move",
+"restriction-protect",
+"restriction-upload",
+"undo-summary",
+"unprotectedarticle-comment"];
+
+##Extensions
+
+/*Extensions usage*/
+$extension_Babel=true;
 ?>
