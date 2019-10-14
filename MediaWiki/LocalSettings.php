6d83b002258ef4745d6895f91881e0992dc45261
PlavorSeol <pseol2190@outlook.com>
2019-10-14 17:40:02 +0900

    Update LocalSettings.php

--- MediaWiki/LocalSettings.php
+++ MediaWiki/LocalSettings.php
@@ -1,6 +1,4 @@
 <?php
-##Debugging
-
 /*
 error_reporting(-1);
 ini_set("display_errors",1);
@@ -8,16 +6,40 @@ $wgShowDBErrorBacktrace=true;
 $wgShowExceptionDetails=true;
 $wgShowSQLErrors=true;
 //*/
-
-##Prevent web access
-
 if (!defined("MEDIAWIKI"))
 {exit("This is not a valid entry point.");}
 
-##Initialize
+#Initialize
 
-/*Directory*/
+/*efGetSiteParams callback*/
+function efGetSiteParams($conf,$wiki)
+{$lang=null;
+$site=null;
+foreach($conf->suffixes as $suffix)
+  {if (substr($wiki,-strlen($suffix))==$suffix)
+    {$lang=substr($wiki,0,-strlen($suffix));
+    $site=$suffix;
+    break;}
+  }
+return
+  ["lang"=>$lang,
+  "params"=>
+    ["lang"=>$lang,
+    "site"=>$site,
+    "wiki"=>$wiki],
+  "suffix"=>$site,
+  "tags"=>
+    []
+  ];
+}
+
+/*Variables*/
+$wmgCacheExpiry=60; //1 minute
+$wmgCentralWiki="wiki";
 $wmgDataDirectory="{$IP}/data";
+$wmgGlobalAccountExemptWikis=[];
+//Should be one of "", "centralauth" and "shared-database"
+$wmgGlobalAccountMode="";
 switch (PHP_OS_FAMILY)
 {case "Linux":
 $wmgPrivateDataDirectory="/plavormind/web_data/mediawiki";
@@ -27,51 +49,31 @@ $wmgPrivateDataDirectory="C:/plavormind/web_data/mediawiki";
 break;
 default:
 $wmgPrivateDataDirectory="{$IP}/private_data";}
+$wmgWiki="wiki";
 
-/*Variable*/
-$wmgCacheExpiry=60; //1 minute
-//Should be one of "", "centralauth" and "shared-database"
-$wmgGlobalAccountMode="centralauth";
-
-/*Wiki*/
-$wmgCentralWiki="exit";
+/*Wiki selector*/
 if ($wgCommandLineMode)
 {if (defined("MW_DB"))
   {$wmgWiki=MW_DB;}
 else
   {exit("Wiki is not specified.");}
 }
-elseif (preg_match("/^([\-\dA-Za-z]+)(?:\.[\-\dA-Za-z]+){2}$/i",parse_url($_SERVER["HTTP_HOST"],PHP_URL_HOST),$matches))
+elseif (preg_match("/^([\d\-a-z]+)(?:\.[\d\-a-z]+){2}$/i",parse_url($_SERVER["HTTP_HOST"],PHP_URL_HOST),$matches))
 {$wmgWiki=$matches[1];}
 else
 {exit("Cannot find this wiki.");}
 
-##System
-
-/*Database*/
-$wgDBname="{$wmgWiki}wiki";
-$wgLocalDatabases=["exitwiki","livewiki","osawiki"];
-if (!in_array($wgDBname,$wgLocalDatabases))
-{exit("Cannot find this wiki.");}
-
-##Wiki settings
-
-/*Load settings*/
-//Core settings
-if (file_exists("{$wmgDataDirectory}/GlobalCoreSettings.php"))
-{include_once("{$wmgDataDirectory}/GlobalCoreSettings.php");}
-if (file_exists("{$wmgDataDirectory}/{$wmgWiki}/CoreSettings.php"))
-{include_once("{$wmgDataDirectory}/{$wmgWiki}/CoreSettings.php");}
-//Extensions and skins settings
-if (file_exists("{$wmgDataDirectory}/GlobalExtraSettings.php"))
-{include_once("{$wmgDataDirectory}/GlobalExtraSettings.php");}
-if (file_exists("{$wmgDataDirectory}/{$wmgWiki}/ExtraSettings.php"))
-{include_once("{$wmgDataDirectory}/{$wmgWiki}/ExtraSettings.php");}
-//Private settings
-if (file_exists("{$wmgPrivateDataDirectory}/PrivateSettings.php"))
-include_once("{$wmgPrivateDataDirectory}/PrivateSettings.php");
-
-/*Permission inheritance*/
+/*Others*/
+require_once($wmgDataDirectory."/GlobalCoreSettings.php");
+if (file_exists($wmgDataDirectory."/".$wmgWiki."/CoreSettings.php"))
+{include_once($wmgDataDirectory."/".$wmgWiki."/CoreSettings.php");}
+if (file_exists($wmgDataDirectory."/GlobalExtraSettings.php"))
+{include_once($wmgDataDirectory."/GlobalExtraSettings.php");}
+if (file_exists($wmgDataDirectory."/".$wmgWiki."/ExtraSettings.php"))
+{include_once($wmgDataDirectory."/".$wmgWiki."/ExtraSettings.php");}
+if (file_exists($wmgPrivateDataDirectory."/PrivateSettings.php"))
+{include_once($wmgPrivateDataDirectory."/PrivateSettings.php");}
+//Permission inheritance
 $wgGroupPermissions["staff"]+=$wgGroupPermissions["autoconfirmed"];
 $wgGroupPermissions["admin"]+=$wgGroupPermissions["staff"];
 $wgGroupPermissions["bureaucrat"]+=$wgGroupPermissions["admin"];
