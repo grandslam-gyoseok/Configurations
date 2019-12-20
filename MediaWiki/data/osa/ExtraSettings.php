f61f0272b4fe18ecd0d4426caedeb4dbf0d51f22
PlavorSeol <pseol2190@outlook.com>
2019-12-21 05:40:13 +0900

    Create ExtraSettings.php

---
+++ MediaWiki/data/osa/ExtraSettings.php
@@ -0,0 +1,13 @@
+<?php
+if (!defined("MEDIAWIKI"))
+{exit("This is not a valid entry point.");}
+
+#Skins
+
+/*PlavorBuma*/
+$wgPBColorClass="is-warning";
+$wgPBUseSidebarOnNavbar=true;
+
+/*Others*/
+$wgDefaultSkin="PlavorBuma";
+?>
