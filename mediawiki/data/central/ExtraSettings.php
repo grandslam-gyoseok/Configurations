cc57d246cfdff4505cb79861339189487585f015
PlavorSeol <pseol2190@outlook.com>
2020-04-27 05:27:23 +0900

    Implement new conventions for comments

--- mediawiki/data/central/ExtraSettings.php
+++ mediawiki/data/central/ExtraSettings.php
@@ -2,8 +2,8 @@
 if (!defined("MEDIAWIKI"))
 {exit("This is not a valid entry point.");}
 
-#Extensions
+//< Extensions >
 
-/*PlavorMindTools*/
+//<< PlavorMindTools >>
 $wgPMTFeatureConfig["BlueCategoryLinks"]["enable"]=true;
 $wgPMTFeatureConfig["UserPageAccess"]["enable"]=true;
