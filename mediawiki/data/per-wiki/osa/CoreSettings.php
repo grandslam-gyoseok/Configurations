92ae35d1ef4f9872db674c59c074ce76734a3486
PlavorSeol <pseol2190@outlook.com>
2021-01-20 19:52:45 +0900

    Apply new coding conventions

--- mediawiki/data/per-wiki/osa/CoreSettings.php
+++ mediawiki/data/per-wiki/osa/CoreSettings.php
@@ -1,39 +1,36 @@
 <?php
-if (!defined("MEDIAWIKI"))
-{exit("This is not a valid entry point.");}
-
 //< General >
 
 //<< Basic information >>
 $wgLogos=
-["1x"=>"/resources/per-wiki/".$wmgWiki."/logos/logo-1x.png",
-"1.5x"=>"/resources/per-wiki/".$wmgWiki."/logos/logo-1.5x.png",
-"2x"=>"/resources/per-wiki/".$wmgWiki."/logos/logo-2x.png",
-"icon"=>"/resources/per-wiki/".$wmgWiki."/logos/logo-2x.png"];
-$wgSitename="오사위키덤프";
+['1x' => "/resources/per-wiki/{$wmgWiki}/logos/logo-1x.png",
+'1.5x' => "/resources/per-wiki/{$wmgWiki}/logos/logo-1.5x.png",
+'2x' => "/resources/per-wiki/{$wmgWiki}/logos/logo-2x.png",
+'icon' => "/resources/per-wiki/{$wmgWiki}/logos/logo-2x.png"];
+$wgSitename='오사위키덤프';
 
 //< Permissions >
 
 //<< User group permissions >>
-$wmgGroupPermissions["*"]["createaccount"]=false;
-$wmgGroupPermissions["user"]["edit"]=false;
+$wmgGroupPermissions['*']['createaccount']=false;
+$wmgGroupPermissions['user']['edit']=false;
 
 //< Extensions >
 
 //<< Extension usage >>
 $wmgExtensions=array_merge($wmgExtensions,
-["Cite"=>true,
-"Highlightjs_Integration"=>true,
-"Josa"=>true,
-"Math"=>true,
-"PageImages"=>true,
-"ParserFunctions"=>true,
-"Popups"=>true,
-"ReplaceText"=>true,
-"SyntaxHighlight_GeSHi"=>true,
-"TextExtracts"=>true]);
+['Cite' => true,
+'Highlightjs_Integration' => true,
+'Josa' => true,
+'Math' => true,
+'PageImages' => true,
+'ParserFunctions' => true,
+'Popups' => true,
+'ReplaceText' => true,
+'SyntaxHighlight_GeSHi' => true,
+'TextExtracts' => true]);
 
 //< Skins >
 
 //<< Skin usage >>
-$wmgSkins["PlavorBuma"]=true;
+$wmgSkins['PlavorBuma']=true;
