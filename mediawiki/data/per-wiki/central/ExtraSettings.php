fc64f83bef51a415cde787b3af694071926451f7
PlavorSeol <pseol2190@outlook.com>
2021-01-20 19:38:49 +0900

    Apply new coding conventions

--- mediawiki/data/per-wiki/central/ExtraSettings.php
+++ mediawiki/data/per-wiki/central/ExtraSettings.php
@@ -1,9 +1,6 @@
 <?php
-if (!defined("MEDIAWIKI"))
-{exit("This is not a valid entry point.");}
-
 //< Extensions >
 
 //<< PlavorMindTools >>
-$wgPMTFeatureConfig["BlueCategoryLinks"]["enable"]=true;
-$wgPMTFeatureConfig["UserPageAccess"]["enable"]=true;
+$wgPMTFeatureConfig['BlueCategoryLinks']['enable']=true;
+$wgPMTFeatureConfig['UserPageAccess']['enable']=true;
