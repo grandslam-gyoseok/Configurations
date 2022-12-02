f2ac5ac9fb7169ca64fc7c151f6f570432d5e0b9
PlavorSeol <pseol2190@outlook.com>
2022-12-03 05:07:53 +0900

    Clarify that $wgMathValidModes is same as default in MediaWiki 1.40 or newer

--- mediawiki/global-settings.php
+++ mediawiki/global-settings.php
@@ -1231,6 +1231,7 @@ if ($wmgUseExtensions['Math']) {
   // This extension requires running update.php.
   wfLoadExtension('Math');
   $wgMathEnableWikibaseDataType = false;
+  // This is same as default in MediaWiki 1.40 or newer.
   $wgMathValidModes = ['mathml', 'source'];
 }
 
