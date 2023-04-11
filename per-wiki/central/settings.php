47ee7c0d33cfa1ca33ae41575183034b8a333ac5
PlavorSeol <pseol2190@outlook.com>
2023-04-12 02:47:57 +0900

    Introduce central-settings.php
    Migrate some settings to central-settings.php

---
+++ per-wiki/central/settings.php
@@ -0,0 +1,76 @@
+<?php
+//< General Settings >
+
+$wgSitename = 'PlavorMind Central';
+
+//< Server URLs and file paths >
+
+$wgLogos = [
+  '1x' => "/resources/per-wiki/$wmgWiki/logos/logo-1x.png",
+  '1.5x' => "/resources/per-wiki/$wmgWiki/logos/logo-1.5x.png",
+  '2x' => "/resources/per-wiki/$wmgWiki/logos/logo-2x.png",
+  'icon' => "/resources/per-wiki/$wmgWiki/logos/logo.svg",
+  'svg' => "/resources/per-wiki/$wmgWiki/logos/logo.svg"
+];
+
+//< Files and file uploads >
+
+//<< Images >>
+
+//<<< Thumbnail settings >>>
+
+// 1.40+
+$wgThumbnailNamespaces = [NS_FILE, NS_HELP, NS_MAIN, NS_PROJECT, NS_USER];
+
+//< ResourceLoader >
+
+$wgAllowSiteCSSOnRestrictedPages = true;
+
+//< Page titles and redirects >
+
+//<< Namespaces >>
+
+$wgMetaNamespace = 'PlavorMind';
+$wgNamespaceAliases = [
+  '@' => NS_USER,
+  'PlavorMind_Central' => NS_PROJECT,
+  'PM' => NS_PROJECT
+];
+
+//< Interwiki links and sites >
+
+$wgLocalInterwikis = ['central'];
+
+//< User rights, access control and monitoring >
+
+//<< Access >>
+
+$wgEnablePartialActionBlocks = true;
+$wgNamespaceProtection = [
+  NS_PROJECT => ['editprotected-admin'],
+  NS_TEMPLATE => ['editprotected-admin']
+];
+// $wgNonincludableNamespaces
+
+//< Copyright >
+
+$wgUseCopyrightUpload = true;
+
+//< Extensions >
+
+//<< CiteThisPage >>
+
+$wgCiteThisPageAdditionalNamespaces[NS_PROJECT] = true;
+
+//<< VisualEditor >>
+
+// Experimental
+$wgVisualEditorAllowExternalLinkPaste = true;
+$wgVisualEditorEnableVisualSectionEditing = true;
+
+//< Skins >
+
+//<< Vector >>
+
+// 1.41+
+$wgVectorZebraDesign = true;
---
+++ per-wiki/osa/extra-usages.php
@@ -0,0 +1,11 @@
+<?php
+$wmgUseExtensions = array_merge($wmgUseExtensions, [
+  'Josa' => true,
+  'Math' => true,
+  'MultimediaViewer' => true,
+  'PageImages' => true,
+  'Popups' => true,
+  'ReplaceText' => true,
+  'SyntaxHighlight_GeSHi' => true,
+  'TextExtracts' => true
+]);
