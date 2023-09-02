2318c821eb0f1ce9726ad0beea58fe1635b32467
PlavorSeol <pseol2190@outlook.com>
2023-09-02 15:39:33 +0900

    Load logos from CDN

--- mediawiki/per-wiki/osa/settings.php
+++ mediawiki/per-wiki/osa/settings.php
@@ -6,10 +6,10 @@ $wgSitename = '오사위키덤프';
 //< Server URLs and file paths >
 
 $wgLogos = [
-  '1x' => "/resources/per-wiki/$wmgWiki/logos/logo-1x.png",
-  '1.5x' => "/resources/per-wiki/$wmgWiki/logos/logo-1.5x.png",
-  '2x' => "/resources/per-wiki/$wmgWiki/logos/logo-2x.png",
-  'icon' => "/resources/per-wiki/$wmgWiki/logos/logo-2x.png"
+  '1x' => "$wmgCDNBaseURL/per-wiki/$wmgWiki/logos/logo-1x.png",
+  '1.5x' => "$wmgCDNBaseURL/per-wiki/$wmgWiki/logos/logo-1.5x.png",
+  '2x' => "$wmgCDNBaseURL/per-wiki/$wmgWiki/logos/logo-2x.png",
+  'icon' => "$wmgCDNBaseURL/per-wiki/$wmgWiki/logos/logo-2x.png"
 ];
 
 //< User rights, access control and monitoring >
